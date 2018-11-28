16c722287963ba92d8d4424ed8cc8a2d78b4db7a
dmisuvorov <dmitrysuvorov9@gmail.com>
2018-11-28 17:58:12 +0300

    write Recorder

--- app/src/main/java/com/media/dmitry68/callrecorder/recorder/ConstantsForRecorder.kt
+++ app/src/main/java/com/media/dmitry68/callrecorder/recorder/ConstantsForRecorder.kt
@@ -1,15 +1,12 @@
-package com.media.dmitry68.callrecorder.preferences
+package com.media.dmitry68.callrecorder.recorder
 
 import android.media.MediaRecorder
 import android.os.Environment
 
-internal object DefaultConstants {
-    val fileName = "Default file"
+internal object ConstantsForRecorder {
     val dirName = "CallRecorder"
     val dirPath = Environment.getExternalStorageDirectory().path
     val audioSource = MediaRecorder.AudioSource.VOICE_COMMUNICATION
     val audioEncoder = MediaRecorder.AudioEncoder.AMR_NB
     val outputFormat = MediaRecorder.OutputFormat.AMR_NB
-    val flagShowDirectionCall = true
-    val flagShowPhoneNumber = true
 }
