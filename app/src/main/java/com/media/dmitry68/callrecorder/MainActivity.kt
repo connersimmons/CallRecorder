ae267597a862aa20d26541960b03d55c44444f08
dmisuvorov <dmitrysuvorov9@gmail.com>
2018-12-04 17:57:10 +0300

    Rework preference and add external module

--- app/src/main/java/com/media/dmitry68/callrecorder/MainActivity.kt
+++ app/src/main/java/com/media/dmitry68/callrecorder/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.media.dmitry68.callrecorder.permissions.PermissionManager
+import com.media.dmitry68.callrecorder.preferences.SettingsActivity
 import com.media.dmitry68.callrecorder.service.CallService
 
 
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onResume() {
-        super.onResume()
+        super.onResume() //TODO: requestPermission on preference change
         Log.d(TAG, "Resume MainActivity")
     }
 
@@ -43,6 +44,11 @@ class MainActivity : AppCompatActivity() {
         ContextCompat.startForegroundService(applicationContext, intent)
     }
 
+    private fun stopCallService() {
+        val intent = Intent().apply { setClass(applicationContext, CallService::class.java) }
+        stopService(intent)
+    }
+
     //TODO: move requestpermission result to permissionManager
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when(requestCode){
@@ -52,16 +58,23 @@ class MainActivity : AppCompatActivity() {
                     put(Manifest.permission.READ_PHONE_STATE, PackageManager.PERMISSION_GRANTED)
                     put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED)
                     put(Manifest.permission.RECORD_AUDIO, PackageManager.PERMISSION_GRANTED)
+                    if (permissionManager.flagPermissionAudioSettings)
+                        put(Manifest.permission.MODIFY_AUDIO_SETTINGS, PackageManager.PERMISSION_GRANTED)
                 }
                 for (i in 0 until permissions.size)
                     perms[permissions[i]] = grantResults[i]
                 if (perms[Manifest.permission.READ_PHONE_STATE] == PackageManager.PERMISSION_GRANTED
                     && perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
                     && perms[Manifest.permission.RECORD_AUDIO] == PackageManager.PERMISSION_GRANTED) {
-                    startCallService()
+                    if (permissionManager.flagPermissionAudioSettings) {
+                        if (perms[Manifest.permission.MODIFY_AUDIO_SETTINGS] == PackageManager.PERMISSION_GRANTED)
+                            startCallService()
+                    } else
+                        startCallService()
                 } else {
                     Toast.makeText(this, R.string.message_problem_with_permission, Toast.LENGTH_LONG)
                         .show()
+                    stopCallService()
                 }
             }
             else -> {
