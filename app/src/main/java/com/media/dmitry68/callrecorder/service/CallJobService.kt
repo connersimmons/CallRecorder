f891dfb6b89df36254b90e96e382583f979f84b6
dmisuvorov <dmitrysuvorov9@gmail.com>
2018-12-13 20:47:13 +0300

    Move SensorReceiver to JobService

--- app/src/main/java/com/media/dmitry68/callrecorder/service/CallJobService.kt
+++ app/src/main/java/com/media/dmitry68/callrecorder/service/CallJobService.kt
@@ -5,8 +5,12 @@ import android.app.job.JobService
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
+import com.media.dmitry68.callrecorder.shaker.ShakeDetector
+import com.media.dmitry68.callrecorder.shaker.ShakeListener
 
 class CallJobService: JobService() {
     private val TAG = "LOG"
@@ -15,17 +19,49 @@ class CallJobService: JobService() {
     override fun onStartJob(params: JobParameters?): Boolean {
         Log.d(TAG, "Start job with id: ${params!!.jobId}")
         jobParameters = params
-        val sensorIntent = Intent(applicationContext, SensorIntentService::class.java)
-            .apply {
-                action = SensorIntentService.START_SENSOR_SERVICE
-            }
-        startService(sensorIntent)
+        startShakeDetector()
         return true
      }
 
     override fun onStopJob(params: JobParameters?): Boolean {
         Log.d(TAG, "Stop job with id: ${params!!.jobId}")
         return true
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        Log.d(TAG, "JobService on task remove")
+        super.onTaskRemoved(rootIntent)
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "JobService destroy")
+        super.onDestroy()
+    }
+
+    private fun startShakeDetector() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE)
+        if (sensorManager is SensorManager){
+            Log.d(TAG, "Start sensor service")
+            val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            val shakeDetector = ShakeDetector()
+            shakeDetector.shakeListener = object : ShakeListener {
+                override fun onShake(count: Int) {
+                    Log.d(TAG, "Detect $count shake")
+                    if (count == 3) {//TODO:make count preference
+                        startIntentService()
+                    }
+                }
+            }
+            sensorManager.registerListener(shakeDetector, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    private fun startIntentService(){
+         val sensorIntent = Intent(applicationContext, SensorIntentService::class.java)
+            .apply {
+                action = SensorIntentService.START_SENSOR_SERVICE
+            }
+        startService(sensorIntent)
     }
 
 
--- app/src/main/java/com/media/dmitry68/callrecorder/service/SensorIntentService.kt
+++ app/src/main/java/com/media/dmitry68/callrecorder/service/SensorIntentService.kt
@@ -4,47 +4,35 @@ import android.app.IntentService
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.hardware.Sensor
-import android.hardware.SensorManager
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.media.dmitry68.callrecorder.notification.NotifyManager
-import com.media.dmitry68.callrecorder.shaker.ShakeDetector
-import com.media.dmitry68.callrecorder.shaker.ShakeListener
 
 class SensorIntentService: IntentService("SensorIntentService"){
     private val TAG = "LOG"
 
     override fun onHandleIntent(intent: Intent?) {
         if (intent!!.action == START_SENSOR_SERVICE){
-            Log.d(TAG, "Start sensor service")
-            startShakeDetector()
+            Log.d(TAG, "Start push notification")
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+            val notification = NotifyManager(applicationContext).builder()
+            if (notificationManager is NotificationManager)
+                notificationManager.notify(NotifyManager.NOTIFICATION_ID, notification.build())
         }
     }
 
-    private fun startShakeDetector() {
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE)
-        if (sensorManager is SensorManager){
-            val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-            val shakeDetector = ShakeDetector()
-            shakeDetector.shakeListener = object : ShakeListener {
-                override fun onShake(count: Int) {
-                    Log.d(TAG, "Detect $count shake")
-                    if (count == 3) {//TODO:make count preference
-                        Log.d(TAG, "Start push notification")
-                        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
-                        val notification = NotifyManager(applicationContext).builder()
-                        if (notificationManager is NotificationManager)
-                            notificationManager.notify(NotifyManager.NOTIFICATION_ID, notification.build())
-                        sendIntentOnFinishJob()
-                    }
-                }
-            }
-            sensorManager.registerListener(shakeDetector, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
-        }
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        Log.d(TAG, "Intent Service on task remove")
+        super.onTaskRemoved(rootIntent)
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "Intent Service destroy")
+        super.onDestroy()
     }
 
     private fun sendIntentOnFinishJob() {
+        Log.d(TAG, "Send intent JOB_FINISH")
         LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(CallJobService.JOB_FINISH))
     }
 
